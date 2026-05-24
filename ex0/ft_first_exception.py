68f2f4b006cf7f451dfc60706ebdf5bffedf8f14
bassaaaa <white.galaxys2.album2@gmail.com>
2026-05-24 13:45:25 +0900

    修正

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -3,34 +3,20 @@ def input_temperature(temp_str: str) -> int:
 
 
 def test_temperature() -> None:
-    input1 = "25"
-    input2 = "abc"
-    try:
-        print(f"Input data is '{input1}'")
-        temp = input_temperature(input1)
-        print(f"Temperature is now {temp}°C\n")
-    except ValueError:
-        print(
-            f"Caught input_temperature error: "
-            f"invalid literal for int() with base 10: '{input1}'\n"
-        )
-
-    try:
-        print(f"Input data is '{input2}'")
-        temp = input_temperature(input2)
-        print(f"Temperature is now {temp}°C\n")
-    except ValueError:
-        print(
-            f"Caught input_temperature error: "
-            f"invalid literal for int() with base 10: '{input2}'\n"
-        )
-
-    print("All tests completed - program didn't crash!")
+    inputs = ["25", "abc"]
+    for input in inputs:
+        try:
+            print(f"Input data is '{input}'")
+            temp = input_temperature(input)
+            print(f"Temperature is now {temp}°C\n")
+        except ValueError as e:
+            print(f"Caught input_temperature error: {e}\n")
 
 
 def main() -> None:
     print("=== Garden Temperature ===\n")
     test_temperature()
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
